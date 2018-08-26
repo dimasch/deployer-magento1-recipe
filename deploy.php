b26ab385b1982cf3ede069718eeacb72b284ad83
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-08-26 14:09:33 +0400

    Added composer install task.

--- deploy.php
+++ deploy.php
@@ -15,3 +15,10 @@ set('writable_mode', 'chmod'); // Using sudo in writable commands
 
 // Hosts
 inventory('hosts.yml');
+
+desc('Composer install...');
+task('deploy:composer:install', function () {
+    run("cd {{release_path}} && composer install -vvv");
+});
+
+before('deploy:cache:clear', 'deploy:composer:install');
